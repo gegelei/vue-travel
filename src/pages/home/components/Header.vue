63ed18d93055e654cdb5d9c8e91e31504589f797
陈海洋 <1559843332@qq.com>
2020-02-12 14:40:35 +0800

    引入iconfont css设置全局变量

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,17 @@
 <template>
     <div class="header">
-      <div class="header-left">返回</div>
-      <div class="header-input">内容城市/景点/游玩主题</div>
-      <div class="header-right">城市</div>
+      <div class="header-left">
+        <div
+          class="iconfont back-icon">&#xe604;</div>
+      </div>
+      <div class="header-input">
+        <span class="iconfont">&#xe641;</span>
+        输入城市/景点/游玩主题
+      </div>
+      <div class="header-right">
+        城市
+        <span class="iconfont">&#xe6aa;</span>
+      </div>
     </div>
 </template>
 
@@ -13,14 +22,18 @@
 </script>
 
 <style scoped lang="stylus">
+  @import "~@/assets/styles/varibles.styl"
   .header
     display flex
     line-height .86rem
-    background #00bcd4
+    background $bgColor
     color #ffffff
     .header-left
       width .64rem
       float left
+      .back-icon
+        text-align center
+        font-size .4rem
     .header-input
       flex 1
       height .62rem
@@ -28,6 +41,7 @@
       margin-top .12rem
       /*margin-bottom .12rem*/
       margin-left .2rem
+      padding-left .2rem
       background #ffffff
       border-radius .1rem
       color #ccc
