bee8f9a2a8957c35d179c27349d57ae8058cc8de
陈海洋 <1559843332@qq.com>
2020-06-30 14:40:35 +0800

    add

--- src/pages/city/components/cityList.vue
+++ src/pages/city/components/cityList.vue
@@ -1,42 +1,43 @@
 <template>
-    <div  class="citylist" ref="wrapper">
-      <div>
-        <div class="area">
-          <div class=" title border-topbottom" >当前城市</div>
-          <div class="list-now">
-            <div class="list-button">
-              <div class="button">{{this.$store.state.city}}</div>
-            </div>
+  <div class="citylist" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class=" title border-topbottom">当前城市</div>
+        <div class="list-now">
+          <div class="list-button">
+            <div class="button">{{this.$store.state.city}}</div>
           </div>
         </div>
-        <div class="area">
-          <div class=" title border-topbottom" >热门城市</div>
-          <div class="list-hot">
-            <div class="list-button"
-                 v-for="item of hotCities"
-                 :key="item.id"
-                 @click="handleNowCity(item.name)"
-            >
-              <div class="button">
-                {{item.name}}
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="area" v-for="(item,key) of cities" :key="key" :ref="key">
-          <div class=" title border-topbottom" >{{key}}</div>
-          <div class="list" >
-            <div class="list-every border-topbottom"  v-for="city of item" @click="handleNowCity(city.name)" >
-              {{city.name}}
+      </div>
+      <div class="area">
+        <div class="title border-topbottom" ref="aaa" @click="haha">热门城市</div>
+        <div class="list-hot">
+          <div class="list-button"
+               v-for="item of hotCities"
+               :key="item.id"
+               @click="handleNowCity(item.name)"
+          >
+            <div class="button">
+              {{item.name}}
             </div>
           </div>
         </div>
       </div>
+      <div class="area" v-for="(item,key) of cities" :key="key" :ref="key">
+        <div class=" title border-topbottom lt">{{key}}</div>
+        <div class="list">
+          <div class="list-every border-topbottom" v-for="city of item" @click="handleNowCity(city.name)">
+            {{city.name}}
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-  import BScroll from 'better-scroll';
+  import BScroll from 'better-scroll'
+
   export default {
     name: 'cityList',
     props: {
@@ -49,24 +50,42 @@
       }
     },
     methods: {
+      haha () {
+        let obj = this.$refs
+        console.log(obj)
+        Object.keys(obj).forEach(function (index) {
+          try {
+            console.log(obj[index][0].getBoundingClientRect().top)
+          } catch (e) {
+          }
+        })
+      },
+      handleScrollx () {
+        console.log('aaa距离顶部高度', this.$refs.aaa.getBoundingClientRect().top)
+      },
+
       handleNowCity (NowCity) {
         //通过store属性的dispatch方法，调用actions中的方法，来改变state中的数据
-        this.$store.dispatch('changeCity', NowCity);
+        this.$store.dispatch('changeCity', NowCity)
       }
     },
-    mounted() {
-      this.scroll = new BScroll (this.$refs.wrapper);
-      this.scroll.scrollToElement(this.$refs['G']);
+    mounted () {
+      this.scroll = new BScroll(this.$refs.wrapper, {
+        click: true
+      })
+      // this.scroll.scrollToElement(this.$refs['G']);
 
+      console.log('aaa距离顶部高度', this.$refs.aaa.getBoundingClientRect().top)
+      window.addEventListener('scroll', this.handleScrollx, true)
     },
     watch: {
-      '$store.state.apl':function(newFlag, oldFlag){
+      '$store.state.apl': function (newFlag, oldFlag) {
         //todo: 点击字母，跳转对应的字母块
         // console.log(this.$refs[newFlag][0]);
         // console.log(this.$refs['G']);
         // this.scroll.scrollToElement(this.$refs['G']);
 
-        this.scroll.scrollToElement(this.$refs[newFlag][0]);
+        this.scroll.scrollToElement(this.$refs[newFlag][0])
 
       },
     }
@@ -84,29 +103,36 @@
     bottom: 0
     overflow hidden
     position: absolute;
+
     .title
       background #eaeaea
       padding-left .2rem
       font-size .28rem
+
     .border-topbottom
       &:before
         border-color #ccc
+
       &:after
         border-color #ccc
+
     .list-hot, .list-now
       overflow hidden
       padding: .1rem
       margin-right: .2rem
+
       .list-button
         width 33.3%
         float left
+
         .button
-            margin .1rem
-            padding .1rem 0
-            line-height .5rem
-            border .01rem solid #ccc
-            border-radius .06rem
-            text-align center
+          margin .1rem
+          padding .1rem 0
+          line-height .5rem
+          border .01rem solid #ccc
+          border-radius .06rem
+          text-align center
+
     .list
       .list-every
         line-height .76rem
--- src/pages/city/components/cityList5.vue
+++ src/pages/city/components/cityList5.vue
@@ -1,123 +1,123 @@
-<template>
-    <div  class="list" ref="wrapper">
-      <div>
-        <div class="area">
-          <div class="title border-topbottom" >当前城市</div>
-          <div class="button-list">
-            <div class="button-wrapper">
-              <div class="button">{{this.$store.state.city}}</div>
-            </div>
-          </div>
-        </div>
-        <div class="area">
-          <div class="title border-topbottom" >热门城市</div>
-          <div class="button-list">
-            <div class="button-wrapper"
-                 v-for="item of hotCities"
-                 :key="item.id"
-                 @click="handleNowCity(item.name)"
-            >
-              <div class="button">
-                {{item.name}}
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="area" v-for="(item,key) of cities" :key="key" :ref="key">
-          <div class="title border-topbottom" >{{key}}</div>
-          <div class="item-list" >
-            <div class="item border-bottom"  v-for="city of item" @click="handleNowCity(city.name)" >
-              {{city.name}}
-            </div>
-          </div>
-        </div>
-<!--        <div class="area"  :ref="'G'" >-->
-<!--          <div class=" title border-topbottom" >AA</div>-->
-<!--          <div class="list" >-->
-<!--            <div class="list-every border-topbottom"  >-->
-<!--             BB-->
+<!--<template>-->
+<!--    <div  class="list" ref="wrapper">-->
+<!--      <div>-->
+<!--        <div class="area">-->
+<!--          <div class="title border-topbottom" >当前城市</div>-->
+<!--          <div class="button-list">-->
+<!--            <div class="button-wrapper">-->
+<!--              <div class="button">{{this.$store.state.city}}</div>-->
 <!--            </div>-->
 <!--          </div>-->
 <!--        </div>-->
-      </div>
-    </div>
-</template>
+<!--        <div class="area">-->
+<!--          <div class="title border-topbottom" >热门城市</div>-->
+<!--          <div class="button-list">-->
+<!--            <div class="button-wrapper"-->
+<!--                 v-for="item of hotCities"-->
+<!--                 :key="item.id"-->
+<!--                 @click="handleNowCity(item.name)"-->
+<!--            >-->
+<!--              <div class="button">-->
+<!--                {{item.name}}-->
+<!--              </div>-->
+<!--            </div>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--        <div class="area" v-for="(item,key) of cities" :key="key" :ref="key">-->
+<!--          <div class="title border-topbottom" >{{key}}</div>-->
+<!--          <div class="item-list" >-->
+<!--            <div class="item border-bottom"  v-for="city of item" @click="handleNowCity(city.name)" >-->
+<!--              {{city.name}}-->
+<!--            </div>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--&lt;!&ndash;        <div class="area"  :ref="'G'" >&ndash;&gt;-->
+<!--&lt;!&ndash;          <div class=" title border-topbottom" >AA</div>&ndash;&gt;-->
+<!--&lt;!&ndash;          <div class="list" >&ndash;&gt;-->
+<!--&lt;!&ndash;            <div class="list-every border-topbottom"  >&ndash;&gt;-->
+<!--&lt;!&ndash;             BB&ndash;&gt;-->
+<!--&lt;!&ndash;            </div>&ndash;&gt;-->
+<!--&lt;!&ndash;          </div>&ndash;&gt;-->
+<!--&lt;!&ndash;        </div>&ndash;&gt;-->
+<!--      </div>-->
+<!--    </div>-->
+<!--</template>-->
 
-<script>
-  import BScroll from 'better-scroll';
-  export default {
-    name: 'cityList',
-    props: {
-      hotCities: Array,
-      cities: ''
-    },
-    data () {
-      return {
-        apl: this.$store.state.apl,
-      }
-    },
-    methods: {
-      handleNowCity (NowCity) {
-        //通过store属性的dispatch方法，调用actions中的方法，来改变state中的数据
-        this.$store.dispatch('changeCity', NowCity);
-      }
-    },
-    mounted() {
-      this.scroll = new BScroll (this.$refs.wrapper);
-      this.scroll.scrollToElement(this.$refs['G']);
+<!--<script>-->
+<!--  import BScroll from 'better-scroll';-->
+<!--  export default {-->
+<!--    name: 'cityList',-->
+<!--    props: {-->
+<!--      hotCities: Array,-->
+<!--      cities: ''-->
+<!--    },-->
+<!--    data () {-->
+<!--      return {-->
+<!--        apl: this.$store.state.apl,-->
+<!--      }-->
+<!--    },-->
+<!--    methods: {-->
+<!--      handleNowCity (NowCity) {-->
+<!--        //通过store属性的dispatch方法，调用actions中的方法，来改变state中的数据-->
+<!--        this.$store.dispatch('changeCity', NowCity);-->
+<!--      }-->
+<!--    },-->
+<!--    mounted() {-->
+<!--      this.scroll = new BScroll (this.$refs.wrapper);-->
+<!--      this.scroll.scrollToElement(this.$refs['G']);-->
 
-    },
-    watch: {
-      '$store.state.apl':function(newFlag, oldFlag){
-        //todo: 点击字母，跳转对应的字母块
-        // console.log(this.$refs[newFlag][0]);
-        // console.log(this.$refs['G']);
-        // this.scroll.scrollToElement(this.$refs['G']);
+<!--    },-->
+<!--    watch: {-->
+<!--      '$store.state.apl':function(newFlag, oldFlag){-->
+<!--        //todo: 点击字母，跳转对应的字母块-->
+<!--        // console.log(this.$refs[newFlag][0]);-->
+<!--        // console.log(this.$refs['G']);-->
+<!--        // this.scroll.scrollToElement(this.$refs['G']);-->
 
-        this.scroll.scrollToElement(this.$refs[newFlag][0]);
+<!--        this.scroll.scrollToElement(this.$refs[newFlag][0]);-->
 
-      },
-    }
-  }
-</script>
+<!--      },-->
+<!--    }-->
+<!--  }-->
+<!--</script>-->
 
-<style scoped lang="stylus">
-  @import "~@styles/varibles.styl"
-  .border-topbottom
-    &:before
-      border-color: #ccc
-    &:after
-      border-color: #ccc
-  .border-bottom
-    &:before
-      border-color: #ccc
-  .list
-    overflow: hidden
-    position: absolute
-    top: 1.58rem
-    left: 0
-    right: 0
-    bottom: 0
-    .title
-      line-height: .54rem
-      background: #eee
-      padding-left: .2rem
-      color: #666
-      font-size: .26rem
-    .button-list
-      overflow: hidden
-      padding: .1rem .6rem .1rem .1rem
-      .button-wrapper
-        float: left
-        width: 33.33%
-        .button
-          margin: .1rem
-          padding: .1rem 0
-          text-align: center
-          border: .02rem solid #ccc
-          border-radius: .06rem
-    .item-list
-      .item
-        line-height: .76rem
-        padding-left: .2rem
-</style>
+<!--<style scoped lang="stylus">-->
+<!--  @import "~@styles/varibles.styl"-->
+<!--  .border-topbottom-->
+<!--    &:before-->
+<!--      border-color: #ccc-->
+<!--    &:after-->
+<!--      border-color: #ccc-->
+<!--  .border-bottom-->
+<!--    &:before-->
+<!--      border-color: #ccc-->
+<!--  .list-->
+<!--    overflow: hidden-->
+<!--    position: absolute-->
+<!--    top: 1.58rem-->
+<!--    left: 0-->
+<!--    right: 0-->
+<!--    bottom: 0-->
+<!--    .title-->
+<!--      line-height: .54rem-->
+<!--      background: #eee-->
+<!--      padding-left: .2rem-->
+<!--      color: #666-->
+<!--      font-size: .26rem-->
+<!--    .button-list-->
+<!--      overflow: hidden-->
+<!--      padding: .1rem .6rem .1rem .1rem-->
+<!--      .button-wrapper-->
+<!--        float: left-->
+<!--        width: 33.33%-->
+<!--        .button-->
+<!--          margin: .1rem-->
+<!--          padding: .1rem 0-->
+<!--          text-align: center-->
+<!--          border: .02rem solid #ccc-->
+<!--          border-radius: .06rem-->
+<!--    .item-list-->
+<!--      .item-->
+<!--        line-height: .76rem-->
+<!--        padding-left: .2rem-->
+<!--</style>-->
