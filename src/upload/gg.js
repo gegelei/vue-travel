ddd0b5095bb8501b074527f7bd64c547e41f4ac2
dahai <1559843332@qq.com>
2020-07-17 17:37:11 +0800

    node 图片上传

---
+++ src/upload/gg.js
@@ -0,0 +1,18 @@
+var fs = require('fs')
+var express = require('express')
+var multer = require('multer')
+
+var app = express()
+var upload = multer({ dest: 'upload/image/' })
+
+// 单图上传
+app.post('/upload', upload.single('logo'), function (req, res, next) {
+  res.send({ ret_code: '0' })
+})
+
+app.get('/form', function (req, res, next) {
+  var form = fs.readFileSync('./form.html', { encoding: 'utf8' })
+  res.send(form)
+})
+
+app.listen(3000)
